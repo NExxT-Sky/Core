120eafcd9164bb1469dafbbedf748300facd75d5
NWJ9PB <helodibalado@gmail.com>
2022-01-08 23:27:59 +0800

    Organize deps + fixed colored logging system

--- functions.js
+++ functions.js
@@ -1,17 +1,11 @@
 /*jshint esversion: 6 */ // This shit will be removed in future
 /*jshint -W061 */ // This shit will be removed in future too BUT if this is not a security issue, this will remain
 
-const chalk = require('chalk');   // Chalk
-const info  = chalk.bold.blue;    // Info
-const error = chalk.bold.red;     // Error
-const warn  = chalk.bold.yellow;  // Warn
-const debug = chalk.bold.green;   // Debug
-const fatal = chalk.bold.red;     // Fatal
-
-// Import Os from index.js
+// Import chalk dependency
+const { chalk, os, table, fs, path } = require('./deps.js');
 
 /**
-* @title NodeLogger
+* @title ConsoleLogger
 * @author NWJ9PB
 * @description With added what action is being logged
 * @version 0.0.2-cuttingedge
@@ -28,126 +22,48 @@ const alog = (type, aclass, message) => {
   
   switch (aclass) {
     case 'node':
-    lclass =  info(' NODE ');
+    lclass =  chalk.bgBlack.blueBright.bold(' NODE ');
     break;
     case 'server':
-    lclass =  warn(' SRV  ');
+    lclass =  chalk.bgBlack.yellow.bold(' SRV  ');
     break;
     case 'client':
-    lclass = debug(' CLT  ');
+    lclass = chalk.bgBlack.white.bold(' CNT  ');
     break;
     case 'database':
-    lclass = error(' DB   ');
+    lclass = chalk.bgBlack.cyan.bold(' DB   ');
     break;
     case 'network':
-    lclass = fatal(' NET  ');
+    lclass = chalk.bgBlack.blue.bold(' NET  ');
     break;
     default:
-    lclass =  info(' LOG  ');
+    lclass =  chalk.bgBlack.black.bold(' LOG  ');
   }
   
   switch (type) {
     case 'info':
-    ltype =  info(' INFO ');
+    ltype =  chalk.bgBlackBright.white.bold(' INFO ');
     break;
     case 'error':
-    ltype = error(' ERROR');
+    ltype = chalk.bgRedBright(' ERROR');
     break;
     case 'warn':
-    ltype =  warn(' WARN ');
+    ltype =  chalk.bgYellow.red.bold(' WARN ');
     break;
     case 'debug':
-    ltype = debug(' DEBUG');
+    ltype = chalk.bgCyan.inverse.bold(' DEBUG');
     break;
     case 'fatal':
-    ltype = fatal(' FATAL');
+    ltype = chalk.bgRedBright(' FATAL');
     break;
     case 'log':
-    ltype =  info(' LOG  ');
+    ltype =  chalk.bgBlackBright(' LOG  ');
     break;
     default:
-    ltype =  info(' INFO ');
+    ltype =  chalk.bgBlackBright.white.bold(' INFO ');
   }
   
-  console.log(chalk.gray('['+ new Date().toISOString() +']') + lclass +chalk.black.bold('=>')+ ltype +chalk.black.bold('=> ')+ message);
-};
-
-/**
-* @title ConsoleLogger 
-* @author NWJ9PB
-* @description With added what action is being logged, This is the new version of the logger, and old one will be removed in future
-* @version 0.0.1-cuttingedge
-* @license MIT
-* 
-* @param {string} log_class - The class of the log, either SYSTEM, NODE, SERVER, REQUEST, DATABASE, NETWORK, FILESYSTEM
-* @param {string} log_type - The type of log to be made, either LOG, INFO, ERROR, WARN, DEBUG, FATAL, ACCESS, SECURITY, CRITICAL
-* @param {string} message - The message to be logged
-*/
-
-// Create a sys_log class and create by doing slog.{log_class}.{log_type}(message)
-const slog = (log_class, log_type, message) => {
-  log_class = log_class.toLowerCase();
-  log_type = log_type.toLowerCase();
-
-  switch (log_class) {
-    case 'system':
-      lclass = chalk.blue.bold            (' SYS ');
-      break;
-    case 'node':
-      lclass = chalk.green.bold           (' NODE');
-      break;
-    case 'server':
-      lclass = chalk.yellow.bold          (' SRV ');
-      break;
-    case 'request':
-      lclass = chalk.cyan.bold            (' REQ ');
-      break;
-    case 'database':
-      lclass = chalk.red.bold             (' DB  ');
-      break;
-    case 'network':
-      lclass = chalk.magenta.bold         (' NET ');
-      break;
-    case 'filesystem':
-      lclass = chalk.white.bold           (' FS  ');
-      break;
-    default:
-      lclass = chalk.redBright.bold       (' LOG ');
-  }
-
-  switch (log_type) {
-    case 'info':
-      ltype = chalk.blue.bold             (' INFO ');
-      break;
-    case 'error':
-      ltype = chalk.red.bold              (' ERR  ');
-      break;
-    case 'warn':
-      ltype = chalk.yellow.bold           (' WARN ');
-      break;
-    case 'debug':
-      ltype = chalk.cyan.bold             (' DBG  ');
-      break;
-    case 'fatal':
-      ltype = chalk.magenta.bold          (' FATAL');
-      break;
-    case 'log':
-      ltype = chalk.blue.bold             (' LOG  ');
-      break;      
-    case 'access':
-      ltype = chalk.green.bold.underline  (' ACC  ');
-      break;
-    case 'security':
-      ltype = chalk.red.bold.underline    (' SEC  ');
-      break;
-    case 'critical':
-      ltype = chalk.red.bold.underline    (' CRIT');
-    default:
-      ltype = chalk.blue.bold             (' INFO ');
-  }
-
-  UtcDate = new Date().toISOString();
-  console.log(chalk.gray('[' + UtcDate + ']') + lclass + chalk.black.bold('=>') + ltype + chalk.black.bold('=> ') + message);
+  console.log(chalk.gray('['+ new Date().toISOString() +']') + lclass +chalk.bgBlack.bold('=>')+ ltype +chalk.bgBlack.bold('=> ')+ message);
 };
 
 /**
@@ -165,30 +81,116 @@ sysConsole.addListener("data", function (d) {
       alog('info', 'server', 'Exiting');
       process.exit(0);
       break;
+    
     case 'rs':
       alog('info', 'server', 'Nodemon restart command received, restarting...');
       process.exit(0);
+
     case 'mem':
-      alog('info', 'node', 'RSS Memory:         ' + chalk.red(process.memoryUsage().rss / 1024 / 1024) + ' MB');
-      alog('info', 'node', 'Heap Used Memory:   ' + chalk.red(process.memoryUsage().heapUsed / 1024 / 1024) + ' MB');
-      alog('info', 'node', 'Heap Total Memory:  ' + chalk.red(process.memoryUsage().heapTotal / 1024 / 1024) + ' MB');
-      // Show machine memory usage
-      alog('info', 'node', 'Machine Memory:     ' + chalk.red(os.totalmem() / 1024 / 1024) + ' MB');
-      
+      // Create a table for memory usage
+      const memTable = new table({
+        head: ['Memory', 'Used', 'Free', 'Total'],
+        colWidths: [10, 20, 20, 20]
+      });
+      // Get memory usage
+      const mem = process.memoryUsage();
+      // Add memory usage to table
+      memTable.push([
+        'Heap',
+        (mem.heapUsed / 1024 / 1024).toFixed(2) + ' MB',
+        'N/A',
+        (mem.heapTotal / 1024 / 1024).toFixed(2) + ' MB'
+      ]);
+      memTable.push([
+        'RSS',
+        (mem.rss / 1024 / 1024).toFixed(2) + ' MB',
+        'N/A',
+        'N/A',
+      ]);
+      memTable.push([
+        'External',
+        (mem.external / 1024 / 1024).toFixed(2) + ' MB',
+        'N/A',
+        'N/A',
+      ]);
+      // Add system memory usage to table
+      memTable.push([
+        'System',
+        'N/A',
+        (os.freemem() / 1024 / 1024).toFixed(2) + ' MB',
+        (os.totalmem() / 1024 / 1024).toFixed(2) + ' MB',
+      ]);
+      // Print table
+      console.log(memTable.toString());
       break;
+    
+    // List out all endpoints files
+    case 'endpoints':
+      // Create a table for endpoints
+      const endpointsTable = new table({
+        head: ['Endpoint', 'Description'],
+        colWidths: [20, 30]
+      });
+      // Get all files in endpoints folder
+      const files = fs.readdirSync('./endpoints');
+      // Loop through files
+      for (let i = 0; i < files.length; i++) {
+        // Get file name
+        const file = files[i];
+        // Get file extension
+        const ext = path.extname(file);
+        // Check if file is a js file
+        if (ext === '.js') {
+          // Get file name without extension
+          const name = path.basename(file, ext);
+          // Add file to table
+          // Description is /{filename}/
+          endpointsTable.push([name, '/' + name]);
+        }
+      }
+
+      // Print table
+      console.log(endpointsTable.toString());
+      break;
+
+    // Intentional fail
+    case 'color.test':
+      // Check if DEBUG is enabled
+      if (process.env.DEBUG) {
+        // Intentional fail
+        alog('fatal', 'server', 'COLOR TEST');
+        alog('error', 'server', 'COLOR TEST');
+        alog('warn', 'server', 'COLOR TEST');
+      } else {
+        // Log error
+        alog('error', 'server', 'This command is not available in production mode');
+      }
+
+      break;
+
     case 'help':
       alog('info', 'node', 'Help');
       alog('info', 'node', 'exit - Exits the program');
       alog('info', 'node', 'help - Shows this help');
       break;
+    
     default:
       alog('info', 'node', 'Unknown command');
       alog('info', 'node', 'Type help for a list of commands');
   }
 });
 
+/**
+ * @title Server_Self_Test
+ * @author NWJ9PB
+ * @description Do a self test on the server
+ * @version 0.0.1-cuttingedge
+ * @license MIT
+ */
+
+// TODO: Takeover the traditional self test and create a new one to make only one line call on index.js
+    
 // Export all the functions
 module.exports = {
   alog,
-  slog
 };
